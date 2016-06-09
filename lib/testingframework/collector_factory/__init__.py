8399abbbe9284e47e25a041d010683f7ec9b814b
Weimin Ma <weimin@sumologic.com>
2016-06-09 16:20:36 -0700

    forgot the collector_factory files

---
+++ lib/testingframework/collector_factory/__init__.py
@@ -0,0 +1,11 @@
+'''
+This module contains everything regarding a Collector installation.
+
+@author: Weimin Ma
+@contact: U{weimin@sumologic.com<mailto:weimin@sumologic.com>}
+@since: 2016-06-09
+'''
+
+__all__ = ['collectorfactory']
+
+from .collectorfactory import CollectorFactory
---
+++ lib/testingframework/collector_factory/collectorfactory.py
@@ -0,0 +1,33 @@
+'''
+This class acts as a factory for LocalCollector and returns corresponding to the underlying OS & architecture.
+
+@author: Weimin Ma
+@contact: U{weimin@sumologic.com<mailto:weimin@sumologic.com>}
+@since: 2016-06-09
+'''
+import os
+import platform
+
+from testingframework.log import Logging
+from testingframework.collector.base import Collector
+from testingframework.collector.local import LocalCollector
+from testingframework.collector.windowslocal import WindowsLocalCollector
+
+class CollectorFactory: 
+     
+    @classmethod
+    def getCollector(self, splunk_home, url=None):     
+        '''
+        This method returns Collector Instance corresponding to underlying architecture
+        '''
+        if(platform.system().upper()=='WINDOWS'):
+            return WindowsLocalCollector(install_home, url)
+        else: #posix
+            return LocalCollector(install__home, url)
+
+    @classmethod
+    def getCollectorClassName(self):
+        if(platform.system().upper()=='WINDOWS'):
+            return WindowsLocalCollector
+        else: #posix
+            return LocalCollector
