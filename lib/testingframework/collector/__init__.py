424654614ff70b4655e37ccdc67b5e97878fbb02
ma-family <ma-family@ma-family-mac.local>
2016-10-10 11:49:51 -0700

    remove more unwatded libs

--- lib/testingframework/collector/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['local', 'windowslocal', 'osxlocal']
-
-from .local import LocalCollector
-from .windowslocal import WindowsLocalCollector
-from .osxlocal import OSXLocalCollector
--- lib/testingframework/misc/__init__.py
+++
@@ -1,5 +0,0 @@
-'''
-@author: Weimin Ma
-@contact: U{weimin@sumologic.com<mailto:weimin@sumologic.com>}
-@since: 2016-05-11
-'''
