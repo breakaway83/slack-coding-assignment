ecaa1a0821979ff6673c0d44d815596951530b5e
Weimin Ma <wma.utd@gmail.com>
2016-10-14 17:08:45 -0700

    add test coverage on files.upload

--- tests/slack_api/conftest.py
+++ tests/slack_api/conftest.py
@@ -16,25 +16,6 @@ from testingframework.connector.base import Connector
 LOGGER = logging.getLogger()
 
 
-def pytest_generate_tests(metafunc):
-    for funcargs in getattr(metafunc.function, 'funcarglist', ()):
-        if 'testname' in funcargs:
-            testname = "%s" % funcargs['testname']
-        metafunc.addcall(funcargs=funcargs, id=testname)
-
-
-def params(funcarglist):
-    """
-    method used with generated/parameterized tests, can be used to decorate
-    your test function with the parameters.  Each dict in your list
-    represents on generated test.  The keys in that dict are the parameters
-    to be used for that generated test
-    """
-    def wrapper(function):
-        function.funcarglist = funcarglist
-        return function
-    return wrapper
-
 @pytest.fixture(scope="session")
 def remote_slack(request):
         '''
--- tests/slack_api/test_files.py
+++ tests/slack_api/test_files.py
@@ -1,27 +1,46 @@
 from testingframework.connector.base import Connector
 from testingframework.log import Logging
+from testingframework.util.fileutils import FileUtils
 from slacktest.util.VerifierBase import VerifierBase
 import pytest
 import time
 import os
 import re
 import json
+import urllib
 from conftest import params
 
 logging = Logging()
 LOGGER = logging.logger
-SLACK_URI = "slack.com/api/files.list"
 verifier = VerifierBase()
+fileutils = FileUtils()
 
 class TestFiles(object):
-    def test_files_list(self, remote_slack, connector_slack):
-        LOGGER.info("Start a metrics query and get the result")
+    @params([
+        { 'slack_uri': 'slack.com/api/files.list', 'file_count': 4, 'testname': 'files.list' },
+    ])
+    def test_files_list(self, remote_slack, connector_slack, slack_uri, file_count, testname):
+        LOGGER.info("List files currently exisit on Slack.")
         restconn = connector_slack
         restconn.update_headers('accept', 'application/json')
         restconn.update_headers('content-type', 'application/json')
         params = {'token': remote_slack.test_token}
-        resp, cont = restconn.make_request("GET", SLACK_URI, urlparam=params)
+        resp, cont = restconn.make_request("GET", slack_uri, urlparam=params)
 
-        pytest.set_trace()
 
-        assert resp['status'] == '200' or resp['status'] == '201'
+    @params([
+        { 'slack_uri_list': 'slack.com/api/files.list', 'slack_uri_upload': 'slack.com/api/files.upload', 'filename': 'Uber.py', 'testname': 'upload Python file' },
+    ])
+    def test_files_upload(self, remote_slack, connector_slack, slack_uri_list, slack_uri_upload, filename, testname):
+        LOGGER.info("Upload a file located at data/files directory.")
+        # upload a lookup file via REST
+        path_to_file = os.path.abspath(os.path.join(os.path.abspath(os.path.curdir), '..', '..', 'data', 'files', filename))
+        content = fileutils.get_file_contents(path_to_file)
+        restconn = connector_slack
+        restconn.update_headers('accept', '*/*')
+        restconn.update_headers('content-type', 'multipart/form-data; image/jpeg')
+        urlparams = {'token': remote_slack.test_token, 'content': content, 'filename': filename}
+        resp, cont = restconn.make_request("POST", slack_uri_upload, urlparam=urllib.urlencode(urlparams))
+        verifier.verify_true(int(resp['status']) == 200)
+        cont_dic = json.loads(cont)
+        verifier.verify_true(cont_dic['ok'])
